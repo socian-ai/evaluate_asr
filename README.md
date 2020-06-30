88952df11bc360ed7830a99a7b69cb1b668a0463
Tareq Al Muntasir <muntasir2000@gmail.com>
2020-06-30 07:12:36 +0600

    inital commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Intro
+The script transcribes audio files in a given directory, calculates WER and generates an HTML report containing diffs, WER etc.
+
+# Setup 
+1. It supports only Python3. First install Python3 if not already installed.
+2. Install virtualenv if not already installed. Run `python3 -m pip install --user virtualenv`
+3. Create a virtual environment for the script. Navigate to the cloned directory and run `python3 -m virtualenv --python=python3 venv` . 
+4. Install Python package dependencies. `python3 -m pip install -r requirements.txt`
+Now you are ready to run the script.
+
+# How to run
+Before running the script, activate the created virtualenv using `source venv/bin/python` (assuming you are in the repository directory where you cloned it). 
+
+## Input params
+- input-dir: The directory where your audio files and their corresponding ground truth transcripts live. Audio files and transcripts must use same filename. For example, audio1.wav file's transcript name must be audio1.txt
+- audio-extension: wav or mp3, default - wav
+- api-endpoint: The API endpoint URL where the ASR API is deployed. default given.
+
+Run `python evaluate_asr.py --input-dir <path-to-your-testset-directory>` for executing the script.
+
+The script will save the predicted transcript text files in `input-dir` with `<audio_file_name>-predicted.txt`.
+
+It will output an HTML file outside of `input-dir` with WER report and diffs between ground truth transcripts and predicted transcripts.
---
+++ evaluate_asr.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+import glob
+import requests
+import Levenshtein as Lev
+from tqdm import tqdm
+import argparse
+from diff_generator import diff_match_patch
+
+dmp = diff_match_patch()
+
+
+input_dir = '/home/tareq/Downloads/testset/testset1_noisy_smartphone_single_speaker'
+audio_extension = 'mp3'
+url = 'http://alap.centralindia.cloudapp.azure.com:8084/transcribe/form/output'
+
+
+
+
+def calculate_wer(hypothesis, ground_truth):
+        """
+        Computes the Word Error Rate, defined as the edit distance between the
+        two provided sentences after tokenizing to words.
+        Arguments:
+            hypothesis (string): space-separated sentence
+            ground_truth (string): space-separated sentence
+        """
+        # build mapping of words to integers
+        b = set(hypothesis.split() + ground_truth.split())
+        word2char = dict(zip(b, range(len(b))))
+
+        # map the words to a char array (Levenshtein packages only accepts
+        # strings)
+        w1 = [chr(word2char[w]) for w in hypothesis.split()]
+        w2 = [chr(word2char[w]) for w in ground_truth.split()]
+
+        distance = Lev.distance(''.join(w1), ''.join(w2))
+        num_words = len(ground_truth.split())
+        return float(distance) / num_words
+
+
+def read_transcript(wav_path):
+    transcript_path = wav_path.replace('.mp3', '.txt')
+    with open(transcript_path, mode='r', encoding='utf8') as f:
+        text = f.read()
+    return text
+
+def write_predicted_transcript(wav_path, text):
+    transcript_path = wav_path.replace('.mp3', '-predicted.txt')
+    with open(transcript_path, mode='w', encoding='utf8') as f:
+        f.write(text)
+    
+
+def write_html_report(results, avg_wer):
+    base_dir = os.path.dirname(input_dir)
+    html_path = os.path.join(base_dir, 'report-'+os.path.basename(input_dir)+'.html')
+    print('Writing report to {}', html_path)
+    print('Please open this file in your browser.')
+    with open(html_path, mode='w', encoding='utf8') as f:
+        f.write('<h3>Average WER on {} files: {:.2f}</h3><br><br>'.format(len(results), avg_wer))
+        for result in results:
+            f.write("Audio: {} <br>".format(result['audio']))
+            f.write("WER: {:.2f} <br>".format(result['WER']))
+            f.write("Transcript: {} <br>".format(result['diff_html']))
+            f.write('<hr style="height:2px;border-width:0;color:gray;background-color:gray"><br>')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input-dir', help='Path to the directory containing wavs and txts', 
+            type=str, required=True)
+    parser.add_argument('--audio-extension', help='Extension of the audio files in input_dir. Supported formats - almost all common formats (wav, mp3, aac etc)', 
+            type=str, default='wav')
+    parser.add_argument('--api-endpoint', help='API Endpoint URL where the ASR is deployed.',
+            type=str, default='http://alap.centralindia.cloudapp.azure.com:8086/transcribe/form/output')
+    
+    params = parser.parse_args()
+
+    input_dir = params.input_dir
+    audio_extension = params.audio_extension
+    url = params.api_endpoint
+
+    audios = glob.glob(os.path.join(input_dir, '*.' + audio_extension))
+    print("Total audio files found: ", len(audios))
+
+    if len(audios) < 1:
+        print('No audio found in dir: ', input_dir)
+        print('Have you specified the correct directory? Or try setting --audio-extension param if your audios are not in wav format')
+        sys.exit()
+
+    total_wer = 0
+    results = []
+    for audio in tqdm(audios):
+        try:
+            print('\nProcessing file: ', audio)
+            post_file = {'file':open(audio, 'rb')}
+            r = requests.post(url, files=post_file)
+            hyp_text = r.json()['transcript']
+            ground_truth = read_transcript(audio)
+            wer = calculate_wer(hyp_text, ground_truth)
+            diff = dmp.diff_main(ground_truth, hyp_text)
+            total_wer += wer
+            results.append({
+                'audio': os.path.basename(audio),
+                'WER': wer,
+                'ground_truth': ground_truth,
+                'predicted': hyp_text,
+                'diff_html': dmp.diff_prettyHtml(diff)
+            })
+            print('Predicted: ', hyp_text)
+            print('Ground Truth: ', ground_truth)
+            print('WER: ', wer)
+
+        except Exception as e:
+            print(e)
+            print('Connection lost. Failed to transcribe ', audio)
+
+    avg_wer = total_wer / len(audios)
+    print("Avg WER on all files: ",avg_wer)
+
+    write_html_report(results, avg_wer)
